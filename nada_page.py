40f4437b7000528e1c96c8bdf5da1abeac31a88f
ebash <ebash@yopmail.com>
2020-02-17 17:52:42 +0200

    added variable for geckodriver for windows platform

--- nada_page.py
+++ nada_page.py
@@ -3,26 +3,32 @@ from selenium.webdriver.common.keys import Keys
 import random
 import urllib.request
 
-driver = webdriver.Firefox(executable_path="/home/john/PycharmProjects/AbSoft/venv/lib/python3.6/site-packages/geckodriver")
+linux_driver_path = ""
+win_driver_path = "geckodriver-v0.24.0-win64/geckodriver.exe"
+
+driver = webdriver.Firefox(executable_path=win_driver_path)
 
 def go_to(addr, title):
     driver.get(addr)
+    print("url: " + str(addr) + "\n" +
+          str(driver.title))
     assert title in driver.title
 
 def find_copy(where_is):
     elem = driver.find_element_by_xpath(where_is)
     elem_data = elem.text
     #add to verification by RegExp
-    print(str(elem_data))
+    print("Finded email now: " + str(elem_data))
     return elem_data
 
-def get_image(from_url, file_path, file_name):
-    full_path = file_path + file_name + ".jpg"
-    urllib.request.urlretrieve(from_url, full_path)
+def get_image(from_url, title, pict_xpath, file_name):
+    go_to(from_url, title)
+    img = driver.find_element_by_xpath(pict_xpath)
+    src = img.get_attribute('src')
+    urllib.urlretrieve(src, "name.extention")
 
-    go_to(from_url+str(random.randint(1, 122))+".jpg")
-    file_name = input("foximage")
-    print()
+
+
 """
 написать функцию для открытия новой вкладки с 'ёпмейл'
 написать письмо с полоченным ссылками и отправить на адрес в гетнада
@@ -43,14 +49,35 @@ title_start_yop = "YOPmail - Disposable Email Address"
 title_start_yop_ru = "YOPmail - Одноразовые, Анонимные  и Бесплатные адреса электронной почты"
 
 mail_field = "/html[1]/body[1]/center[1]/div[1]/div[1]/div[3]/table[3]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[3]/td[1]/div[1]/form[1]/table[1]/tbody[1]/tr[1]/td[2]/input[2]"
-
 mail_addr_yop = "send_image"
-
 mail_get_button = "/html[1]/body[1]/center[1]/div[1]/div[1]/div[3]/table[3]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[3]/td[1]/div[1]/form[1]/table[1]/tbody[1]/tr[1]/td[4]/input[1]"
 
+#cat resources
+cat_url = "https://random.cat/"
+cat_title = "random.cat -- meeeeeeeeoooooooow"
+cat_xpath = "//img[@id='cat']"
+
+#dog resources
+dog_url = "https://random.dog/"
+dog_title = "random.dog"
+dog_xpath = "//img[@id='dog-img']"
+
+
+#fox resources
+fox_url = "https://randomfox.ca/"
+fox_title = "RandomFox"
+fox_xpath = "//a[@id='fox_full_link']"
 
 
 #run start here
+#get cat image, linck for sending
+go_to(cat_url, cat_title)
+
+go_to(dog_url, dog_title)
+
+go_to(fox_url, fox_title)
+
+#get email for sending and ferification
 go_to(url_nada, title_nada)
 find_copy(element)
 
