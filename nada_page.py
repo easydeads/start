408e8531489d66fe62e4779514d7529c797f02bd
ebash <ebash@yopmail.com>
2020-03-11 16:50:07 +0200

    api complete

--- nada_page.py
+++ nada_page.py
@@ -1,11 +1,11 @@
-import urllib.request
+import urllib, json, requests
 from email.mime.multipart import MIMEMultipart
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 opts = Options()
-opts.headless = False
+opts.headless = True
 #opts.set_headless()
 #assert opts.headless
 
@@ -15,76 +15,76 @@ win_driver_path = "geckodriver-v0.24.0-win64/geckodriver.exe"
 driver = webdriver.Firefox(executable_path=win_driver_path)
 
 def go_to(addr, title="Not have"):
-    driver.get(addr)
-    #print("url: " + str(addr) + " title: " + str(driver.title))
-    if title == "Not have":
-        print("Title not have")
-    elif title != "Not have" and type(title) != 'list':
-        list_title = title.split()
-        assert list_title[0] in driver.title
-    else:
-        assert title.split() in driver.title
+  driver.get(addr)
+  #print("url: " + str(addr) + " title: " + str(driver.title))
+  if title == "Not have":
+    print("Title not have")
+  elif title != "Not have" and type(title) != 'list':
+    list_title = title.split()
+    assert list_title[0] in driver.title
+  else:
+    assert title.split() in driver.title
 
 def find_copy(where_is):
-    elem = driver.find_element_by_xpath(where_is)
-    elem_data = elem.text
-    #add to verification by RegExp
-    print("Finded email now: " + str(elem_data))
-    return elem_data
+  elem = driver.find_element_by_xpath(where_is)
+  elem_data = elem.text
+  #add to verification by RegExp
+  print("Finded email now: " + str(elem_data))
+  return elem_data
 
 # проблема возника при извлечении адреса для контента на странице с собаками.
 # див в котором находится контент не статичен и меняется в зависимочти от типа контента
 def get_image_src(from_url, title, x_path):
-    """x_path take as input string or list max from two elements if x_path not static"""
-    """
-    :param from_url:
-    :param title:
-    :param pict_xpath:
-    :return:
-    """
-    go_to(from_url, title)
-    # get image source
-    if type(x_path) == list:
-        try:
-            img = driver.find_element_by_xpath(x_path[0])
-            src = str(img.get_attribute('src'))
-        except:
-            img = driver.find_element_by_xpath(x_path[1])
-            src = str(img.get_attribute('src'))
-    elif type(x_path) == str:
-        img = driver.find_element_by_xpath(x_path)
-        src = str(img.get_attribute('src'))
-    else:
-        print("Unknown type")
+  """x_path take as input string or list max from two elements if x_path not static"""
+  """
+  :param from_url:
+  :param title:
+  :param pict_xpath:
+  :return:
+  """
+  go_to(from_url, title)
+  # get image source
+  if type(x_path) == list:
+    try:
+      img = driver.find_element_by_xpath(x_path[0])
+      src = str(img.get_attribute('src'))
+    except:
+      img = driver.find_element_by_xpath(x_path[1])
+      src = str(img.get_attribute('src'))
+  elif type(x_path) == str:
+    img = driver.find_element_by_xpath(x_path)
+    src = str(img.get_attribute('src'))
+  else:
+    print("Unknown type")
 
-    """if type(src) != 'str':
-        src = img.get_attribute('url')
-        print("Picture url: " + str(src))
-    else:
-        print("Picture src: " + str(src))
-    """
-    print("src is: " + str(src))
-    return src
+  """if type(src) != 'str':
+      src = img.get_attribute('url')
+      print("Picture url: " + str(src))
+  else:
+      print("Picture src: " + str(src))
+  """
+  print("src is: " + str(src))
+  return src
 
 def save_screen():
-    driver.save_screenshot(img.get_attribute('src').split("/")[-1], )
-    print("Saved screen: " + str(img.get_attribute('src').split("/")[-1]))
+  driver.save_screenshot(img.get_attribute('src').split("/")[-1], )
+  print("Saved screen: " + str(img.get_attribute('src').split("/")[-1]))
 
-    # download the image
-    #urllib.urlretrieve(src, title)
+  # download the image
+  #urllib.urlretrieve(src, title)
 
-    # take screenshot
+  # take screenshot
 
 def open_mailbox(url, title, login, input_text, button):
-    go_to(url, title)
-    mail_form = driver.find_element_by_id(login).send_keys(input_text)
-    print("Text: " + input_text)
-    next_button = driver.find_element_by_xpath(button).click()
-    print("Button pressed")
+  go_to(url, title)
+  mail_form = driver.find_element_by_id(login).send_keys(input_text)
+  print("Text: " + input_text)
+  next_button = driver.find_element_by_xpath(button).click()
+  print("Button pressed")
 
 def write_mail():
-    write = driver.find_element_by_xpath("//a[@id='wrmail']").click()
-    to = driver.find_element_by_xpath("//input[@id='mailto']").click()
+  write = driver.find_element_by_xpath("//a[@id='wrmail']").click()
+  to = driver.find_element_by_xpath("//input[@id='mailto']").click()
 
 # google api
 import base64
@@ -131,63 +131,64 @@ def CreateMessage(sender, to, subject, message_text):
   return {'raw': base64.urlsafe_b64encode(message.as_string())}
 
 def create_message_without_attachment (sender, to, subject, msgHtml, msgPlain):
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = to
-    msg.attach(MIMEText(msgPlain, 'plain'))
-    msg.attach(MIMEText(msgHtml, 'html'))
+  msg = MIMEMultipart('alternative')
+  msg['Subject'] = subject
+  msg['From'] = sender
+  msg['To'] = to
+  msg.attach(MIMEText(msgPlain, 'plain'))
+  msg.attach(MIMEText(msgHtml, 'html'))
 
-    raw = base64.urlsafe_b64encode(msg.as_bytes())
-    raw = raw.decode()
-    body = {'raw': raw}
-    return body
+  raw = base64.urlsafe_b64encode(msg.as_bytes())
+  raw = raw.decode()
+  body = {'raw': raw}
+  return body
 
 def send_email(subject, msg, to):
-    import smtplib
-    import config
-    try:
-        server = smtplib.SMTP('smtp.gmail.com:587')
-        server.ehlo()
-        server.starttls()
-        server.login(config.EMAIL_ADDRESS, config.PASSWORD)
-        message = 'Subhect: {}\n\n{}'.format(subject, msg)
-        server.sendmail(config.EMAIL_ADDRESS, to, message)
-        server.quit()
-        print("Success: Email sent!")
-    except:
-        print("Email failed to send")
+  import smtplib
+  import config
+  try:
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.ehlo()
+    server.starttls()
+    server.login(config.EMAIL_ADDRESS, config.PASSWORD)
+    message = 'Subhect: {}\n\n{}'.format(subject, msg)
+    server.sendmail(config.EMAIL_ADDRESS, to, message)
+    server.quit()
+    print("Success: Email sent!")
+  except:
+    print("Email failed to send")
 
-"""
-написать функцию для открытия новой вкладки с 'ёпмейл'
-написать письмо с полоченным ссылками и отправить на адрес в гетнада
-обновить страницу гетнада (при необходимости) и проверить что ссылки соответствубт отправленным
-кликом в ссылки открыть пикчи и сделать скрины открытых страниц/вкладок
-"""
+def src_from_api(url):
+  responce = requests.get(url)
+  data = responce.json()
+  print(data['url'])
 
 #------ VARIABLES start---------
-    #get nada data
+#get nada data
 url_nada = "https://getnada.com"
 title_nada = "Nada - temp mail - fast and free"
 element = "/html[1]/body[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/h1[1]/span[2]"
 fox_image = ("https://randomfox.ca/images/")
 
-    #cat resources
+#cat resources
 cat_url = "https://random.cat/"
+cat_api = "http://aws.random.cat/meow"
 cat_title = "random.cat"
 cat_xpath = "//img[@id='cat']"
 
-    #dog resources
+#dog resources
 dog_url = "https://random.dog/"
+dog_api = "https://random.dog/woof.json"
 dog_title = "random.dog"
 dog_xpath = ["//img[@id='dog-img']", '/html/body/div/div/div/video/source']
 
-    #fox resources
+#fox resources
 fox_url = "https://randomfox.ca/"
+fox_api = "https://randomfox.ca/floof/"
 fox_title = "RandomFox"
 fox_xpath = "//img[@id='fox_img_link']"
 
-    #yopmail data
+#yopmail data
 url_yop = "http://www.yopmail.com/en/"
 title_start_yop = "YOPmail - Disposable Email Address"
 # если есть несколько тайтлов можно попробовать массив, но допилив go_to() "YOPmail - Одноразовые, Анонимные  и Бесплатные адреса электронной почты"
@@ -205,6 +206,7 @@ msgHtml = r'Hi<br/>Html <b>hello</b>'
 msgPlain = "Hi\nPlain Email"
 message_text = "this is message text"
 
+all_data = [fox_api, dog_api, cat_api]
 #------ VARIABLES finish---------
 
 
@@ -213,18 +215,17 @@ message_text = "this is message text"
 #get cat image, linck for sending
 from time import sleep
 
-while True:
-    cat = get_image_src(cat_url, cat_title, cat_xpath)
-    dog = get_image_src(dog_url, dog_title, dog_xpath)
-    fox = get_image_src(fox_url, fox_title, fox_xpath)
-    #msg = str(cat), str(dog), str(fox)
-    print(str(dog))
+#cat = get_image_src(cat_url, cat_title, cat_xpath)
+#dog = get_image_src(dog_url, dog_title, dog_xpath)
+#fox = get_image_src(fox_url, fox_title, fox_xpath)
+#msg = str(cat), str(dog), str(fox)
+for i in all_data:
+  src_from_api(i)
 
-    """send_email("Take this pictures", msg, to)
+"""send_email("Take this pictures", msg, to)
     #get email for sending and ferification
     go_to(url_nada, title_nada)
     find_copy(element)
 """
-    #driver.close()
-    sleep(1)
+#driver.close()
 driver.close()
